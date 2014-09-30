66d6d4918e743baa0e800e6b5d7aaeb590af52b7
Aaron Levie <aaron@MacBook-Pro-2.local>
2014-09-29 22:28:48 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a script I wrote to run on my kid's Linux PC. It allows me to type on my computer or phone
+via SSH and the text is output to the PC. It has completely convinced my son his computer is 
+alive. It is a lot of fun. I have borrowed snippets from forums. Feel free to use and improve it! 
---
+++ clarence.py
@@ -0,0 +1,64 @@
+import cmd
+import os
+import subprocess
+import sys
+
+
+class CLI(cmd.Cmd):
+  def __init__(self):
+    cmd.Cmd.__init__(self)
+    self.prompt = 'Clarence> '
+
+  def do_exit(self, line):
+    print 'Goodbye!'
+    return True
+
+  def do_intro(self, line):
+    self.say('Hello. My name is Clarence.')
+
+  def do_version(self, line):
+    self.say('I am Clarence 2.0')
+
+  def default(self,line=''):
+    self.say(line)
+
+  def say(self,line):
+    line = line.lower()
+    line = line.replace('adalyn','addalynn') # Use these lines to teach clarence to pronunciation
+    args = ['espeak', '-v', 'en-us', '-a', '200', '-s', '150', '"%s"' % line]
+    subprocess.call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def which(program):
+  def is_exe(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+  fpath, fname = os.path.split(program)
+  if fpath:
+    if is_exe(program):
+      return program
+  else:
+    for path in os.environ["PATH"].split(os.pathsep):
+      path = path.strip('"')
+      exe_file = os.path.join(path, program)
+      if is_exe(exe_file):
+        return exe_file
+  return None
+
+
+def main():
+  os.system('clear')
+  if which('espeak') is None:
+    sys.exit('espeak is not installed')
+  if not os.geteuid() == 0:
+    sys.exit('Script must be run as root')
+  try:
+    foo = CLI()
+    foo.cmdloop()
+  except: #catch ctrl-c event to exit
+    print ''
+    pass
+
+
+if __name__ == '__main__':
+  main()
+  
